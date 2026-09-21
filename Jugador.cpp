f7b9d7faee06d4b9858bacfbef0298a696d39e5e
wmarin0agudelo-Marin <wmarin0agudelo@gmail.com>
2026-09-20 21:30:12 -0500

    Update Jugador.cpp

--- Jugador.cpp
+++ Jugador.cpp
@@ -5,3 +5,7 @@ Jugador::Jugador(int id_) : id(id_), puntos(0) {}
 void Jugador::agregarCarta(const Carta& carta) {
   mano.push_back(carta);
 }
+Carta Jugador::jugarCarta(const Condicion& condicion) {
+  if (mano.empty()) {
+        throw std::runtime_error("El jugador no tiene cartas en la mano.");
+    }
