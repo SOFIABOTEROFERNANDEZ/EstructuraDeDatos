ec743ff34cb9251f67833efec9da2849502fd928
wmarin0agudelo-Marin <wmarin0agudelo@gmail.com>
2026-09-20 20:58:46 -0500

    Jugador.h

---
+++ jugador.h
@@ -0,0 +1,29 @@
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include <vector>
+#include "Carta.h"
+#include "Condicion.h"
+
+class Jugador {
+private:
+    int id;
+    std::vector<Carta> mano;
+    int puntos;
+
+public:
+    // Constructor
+    Jugador(int id_);
+
+    // Métodos principales segun UML y requerimientos
+    void agregarCarta(const Carta& carta);
+    Carta jugarCarta(const Condicion& condicion);
+    void sumarPuntos();
+    
+    // Getters y utilidades
+    int getId() const;
+    int getPuntos() const;
+    const std::vector<Carta>& getMano() const;
+};
+
+#endif
