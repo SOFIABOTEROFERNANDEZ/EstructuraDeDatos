bf820ec47767fe1f847659182f8bc56f1c1c2de7
lacevedor <lacevedoacademico@gmail.com>
2026-09-21 21:13:21 -0500

    Refactor Condicion constructor and methods
    ajuste del codigo

--- Condición.cpp
+++ Condición.cpp
@@ -1,12 +1,17 @@
 #include "Condicion.h"
 
-Condicion::Condicion(Color color, TipoOrden tipo)
-    : color(color), tipo(tipo) {}
+Condicion::Condicion()
+    : color(""), orden(TipoOrden::MAYOR) {
+}
 
-Color Condicion::getColor() const {
+Condicion::Condicion(const std::string& color, TipoOrden orden)
+    : color(color), orden(orden) {
+}
+
+std::string Condicion::getColor() const {
     return color;
 }
 
-TipoOrden Condicion::getTipo() const {
-    return tipo;
+TipoOrden Condicion::getOrden() const {
+    return orden;
 }
