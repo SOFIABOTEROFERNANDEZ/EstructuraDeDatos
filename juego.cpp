b9e1822fd456badf7d079b0db83a72140ba0b543
SOFIABOTEROFERNANDEZ <149006514+SOFIABOTEROFERNANDEZ@users.noreply.github.com>
2026-09-22 14:09:06 -0500

    Update and rename main.cpp to juego.cpp

---
+++ juego.cpp
@@ -0,0 +1,15 @@
+#include "Juego.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+Juego::Juego(int jugadorInicial, int rondaActual)
+    : rondaActual(0),
+      jugadorInicial(jugadorInicial),
+      totalRondas(rondaActual),
+      ultimoGanadorIndice(-1)
+{
+}
+
+void Juego::iniciarJuego()
