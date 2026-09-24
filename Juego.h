37b35aa1d1fc41a753994a8cff88b5ed445ba375
wmarin0agudelo-Marin <wmarin0agudelo@gmail.com>
2026-09-23 22:30:51 -0500

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -7,3 +7,19 @@
 #include "Baraja.h"
 #include "Jugador.h"
 #include "Ronda.h"
+
+class Juego {
+private:
+    std::vector<Jugador> jugadores;
+    Baraja baraja;
+    int rondaActual;
+    int jugadorInicial;
+    std::vector<std::string> colores;
+    int maxNumero;
+    bool colorDisponible(
+        const std::string& color
+    ) const;
+    Condicion pedirCondicion() const;
+    void mostrarMano(
+        const Jugador& jugador
+    
