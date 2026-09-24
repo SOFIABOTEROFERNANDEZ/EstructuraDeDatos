841930dca29ed138ac0ee3c3958c2947d279ff25
wmarin0agudelo-Marin <wmarin0agudelo@gmail.com>
2026-09-23 19:54:11 -0500

    Update Jugador.cpp

--- Jugador.cpp
+++ Jugador.cpp
@@ -10,35 +10,40 @@ Jugador::Jugador(int id)
 }
 void Jugador::agregarCarta(const Carta& carta) {
     mano.push_back(carta);
+
 }
-
-Carta Jugador::jugarCarta(const Condicion& condicion) {
+Carta Jugador::seleccionarCarta(
+    const Condicion& condicion
+) const {
     if (mano.empty()) {
-        throw std::runtime_error("El jugador no tiene cartas en la mano.");
+        throw std::runtime_error(
+            "El jugador no tiene cartas."
+        );
     }
-
     int mejorIndice = -1;
-
-    // Buscar entre las cartas de la mano las que coincidan con el color de la condicion
-    for (size_t i = 0; i < mano.size(); ++i) {
-        if (mano[i].getColor() == condicion.getColor()) {
-            if (mejorIndice == -1) {
-                mejorIndice = static_cast<int>(i);
-            } else {
-                bool esMejor = false;
-                if (condicion.getTipo() == TipoOrden::MAS_ALTA) {
-                    esMejor = mano[i].obtenerNumero() > mano[mejorIndice].obtenerNumero();
-                } else { // MAS_BAJA
-                    esMejor = mano[i].obtenerNumero() < mano[mejorIndice].obtenerNumero();
-                }
-
-                if (esMejor) {
-                    mejorIndice = static_cast<int>(i);
-                }
-            }
+    for (int i = 0; i < static_cast<int>(mano.size()); i++) {
+        if (mano[i].getColor() != condicion.getColor()) {
+            continue;
+        }
+        if (indice == -1) {
+            indice = i;
+        }
+        else if (
+            condicion.getOrden() == TipoOrden::MAYOR &&
+            mano[i].getNumero() >
+            mano[indice].getNumero()
+        ) {
+            indice = i;
+        }
+        else if (
+            condicion.getOrden() == TipoOrden::MENOR &&
+            mano[i].getNumero() <
+            mano[indice].getNumero()
+        ) {
+            indice = i;
         }
     }
-
+    
     // Si no tiene cartas del color pedido
     if (mejorIndice == -1) {
         mejorIndice = 0;
